16e5d0d92074d4989f07bb44d05dffb2db54a1a5
AidenGutierrez <113128555+AidenGutierrez@users.noreply.github.com>
2022-12-07 20:23:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,77 @@
 import turtle as trtl
 import time
+#initializes the background screen
 wn = trtl.Screen()
-
-wn.screensize(canvwidth=500,canvheight=300,bg="black")
-#starts by asking user for their name
-Inputname = trtl.textinput("name", "Please insert your name.")
-#initializes turtles
-phoneDrawer = trtl.Turtle()
-phoneDrawer.speed(0)
-friendTextDrawer = trtl.Turtle()
-friendTextDrawer.speed(0)
-friendTextDrawer.hideturtle()
-def textMessage(UserName):
-    #globals the friendTextDrawer for later
-    global friendTextDrawer
-
-    #sets up the drawer
-    global phoneDrawer
-    phoneDrawer.penup()
-    phoneDrawer.fillcolor("white")
-    phoneDrawer.goto(-320,250)
-    phoneDrawer.begin_fill()
-    phoneDrawer.pendown()
-    #draws the rectangle for the phone
-    for i in range(2):
-        phoneDrawer.forward(640)
-        phoneDrawer.right(90)
-        for x in range(1):
-            phoneDrawer.forward(700)
-            phoneDrawer.right(90)
-    phoneDrawer.end_fill()
-    phoneDrawer.hideturtle()
-    #draws the friendTexts on screen
-    friendTextDrawer.penup()
-    friendTextDrawer.color("blue")
-    friendTextDrawer.goto(-300,50)
-    friendTextDrawer.write("Alex: \nHey " + UserName + ", \nwanna hang out today?", font=('Arial',20,'bold'))
-    #draws the new text
-    friendTextDrawer.goto(50,-100)
-    friendTextDrawer.color("green")
-    friendTextDrawer.write("Me: \nSure! Let me ask \nmy mom!",font=('Ariel',20, 'bold'))
-#calls on the name function
-textMessage(Inputname)
-time.sleep(1)
-friendTextDrawer.clear()
-phoneDrawer.clear()
-#makes the user input family names
+wn.bgcolor("black")
+wn.bgpic("PakistanFlag.gif")
+#creates the list of family members 
 familyNamesList = []
-for i in range(2):
-    familynames = trtl.textinput("familyNames", "Please input a sibling names.")
-    familyNamesList.append(familynames)
-print(familyNamesList)
-#puts the mom picture in a variable
-angryMomPic = "AngryMom.gif"
-#creates the scene with angry mom
-def momScene(familylist):
-    #globals the family name
-    index = 0
-    #draws the mom pic on the screen as a background
-    wn.bgpic(angryMomPic)
-    familymember1 = familylist.pop(index)
-    #sets up the text drawer
-    selfTextDrawer = trtl.Turtle()
-    selfTextDrawer.penup()
-    selfTextDrawer.color('white')
-    selfTextDrawer.goto(0,0)
-    selfTextDrawer.write("No Mijo!\n you have to make your siblings Happy!", font=('Arial',20,'bold'))
+for i in range (2):
+    familyNames = trtl.textinput("family names", "Please input a family name:")
+    familyNamesList.append(familyNames)
+#makes the writer
+writer = trtl.Turtle()
+writer.color("red")
+writer.hideturtle()
+writer.penup()
+writer.goto(-450,100)
+#writes the users objective
+writer.write('''Your goal is to reach 
+Pakistan with your family:
+\n'''+ familyNamesList[0] + " and " + familyNamesList[1], font=("Arial",30,'bold'))
+time.sleep(3)
 
-#have noth siblings on screen that require both of them to be at 100% happy at the same time
-#increase happiness by clicking on them a certain amount of times
-#happiness level of the babies can go over 100%
-#creates the sibling screen
-def siblingScreen():
-    wn.bgcolor("white")
+writer.clear()
+#gives the user the objective for the minigame
+writer.write('''Protect you and your family \nfrom the turtles for 10 seconds, 
+\nyou must eliminate at least 15 \nturtles to keep your family alive!''',font=("Arial",30,'bold'))
+time.sleep(4)
+writer.clear()
+#draws the screen for the player to play on
+wn.bgpic("nopic")
+wn.bgcolor("brown")
+legLength = 40
+armLength = 20
+bodyLength = 60
+HeadSize = 40
+xcor = -100
+color = "black"
+#draws each stick figure in different locations
+painter = trtl.Turtle()
+painter.hideturtle()
+painter.speed(0)
+painter.fillcolor(color)
+for people in range(3):
+    #prepares the painter to go to the right spot
+    painter.penup()
+    painter.goto(xcor,0)
+    painter.setheading(0)
+    painter.pendown()
+    #draws head
+    painter.begin_fill()
+    painter.circle(HeadSize)
+    painter.end_fill()
+    #draws body
+    painter.right(90)
+    painter.forward(bodyLength)
+    #draws left leg
+    painter.right(45)
+    painter.forward(legLength)
+    painter.back(legLength)
+    #draws right leg
+    painter.left(90)
+    painter.forward(legLength)
+    painter.back(legLength)
+    painter.right(45)
+    #draws arms in the middle of the body
+    painter.back(bodyLength/2)
+    painter.right(90)
+    painter.forward(armLength)
+    painter.back(armLength*2)
+    xcor += 100
 
 
 
 
 
-momScene(familyNamesList)
-siblingScreen()
 wn.mainloop()
